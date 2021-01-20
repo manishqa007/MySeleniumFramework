6a9d38b2fee0372c2ecbe170e243ae0164eea031
Manish Patel <manishpatel@DLLA43P3273.qait.com>
2021-01-20 17:51:41 +0530

    first commit

---
+++ src/test/java/Demo/bluestack/Keywords/GameTvHomePageAction.java
@@ -0,0 +1,15 @@
+package Demo.bluestack.Keywords;
+
+import org.openqa.selenium.WebDriver;
+
+import Demo.bluestack.GetPageObject.GetPage;
+
+public class GameTvHomePageAction extends GetPage{
+
+	public GameTvHomePageAction(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
